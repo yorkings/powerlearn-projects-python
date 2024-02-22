51d3c013d6021bff0c41a13542b59de2566d00ec
yorkings <yorkingsnyakundi2700@gmail.com>
2024-02-22 19:14:47 +0300

    adding list

---
+++ my_list.py
@@ -0,0 +1,14 @@
+my_list = []       # Create an empty list
+
+  # Append the list [10, 20, 30, 40] to my_list
+my_list.append(10) 
+my_list.append(20) 
+my_list.append(30) 
+my_list.append(40) 
+
+my_list.insert(1, 15)  # Insert the value 15 at index 1
+my_list.pop()       # Remove the last element of the list
+my_list.sort()      # Sort the list in ascending order
+
+print(my_list)
+
